78c68c7dba68b69d2f62cfdc4954a9493ed763ad
mxordn <m.heffter@me.com>
2021-04-17 22:44:35 +0200

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { MatTabsModule } from '@angular/material/tabs';
+
 
 import { AppComponent } from './app.component';
+import { ScoreCanvasComponent, Svg } from 'src/app/score-canvas/score-canvas.component';
+import { ScoreFrameComponent } from 'src/app/score-frame/score-frame.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { InlineSVGModule } from 'ng-inline-svg';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ScoreCanvasComponent,
+    ScoreFrameComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatTabsModule,
+    Svg,
+    InlineSVGModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
